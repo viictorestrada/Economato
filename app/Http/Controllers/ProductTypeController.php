4a0e597b8ef778be89de93e9b33b3f08049a52ae
McTraque <tetidim@gmail.com>
2018-05-10 13:50:00 -0500

    Change validation text to string

--- app/Http/Controllers/ProductTypeController.php
+++ app/Http/Controllers/ProductTypeController.php
@@ -13,7 +13,7 @@ class ProductTypeController extends Controller
     {
       $this->validate($request, [
         'product_type_name' => 'required|string|max:45|unique:product_type_name',
-        'description' => 'required|text'
+        'description' => 'required|string'
       ]);
       ProductType::create($request->all());
       return redirect('configurations')->with([swal()->autoclose(1500)->success('Registro Exitoso', 'Se ha agregado un nuevo registro!')]);
@@ -30,7 +30,7 @@ class ProductTypeController extends Controller
     {
       $this->validate($request, [
         'product_type_name' => 'required|string|max:80|unique:product_type_name', Rule::unique('product_type')->ignore($this->id, 'id'),
-        'description' => 'required|text',
+        'description' => 'required|string',
       ]);
       $measure = ProductType::find($id);
       $measure->update($request->all());
