a71b58eb6fa17054c5d26227c09fb95d816431e1
viictorestrada <v9312@hotmail.com>
2018-06-01 08:00:03 -0500

    LoginController modified

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -45,11 +45,4 @@ class LoginController extends Controller
     return redirect('/');
   }
 
-  protected function credentials(Request $request)
-  {
-    $credentials = $request->only($this->username(), 'password');
-    $credentials['status'] = 1;
-    return $credentials;
-  }
-
 }
