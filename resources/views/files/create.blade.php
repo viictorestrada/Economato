a98bf6edf2445caed6ce80d9942b074600b6a577
McTraque <tetidim@gmail.com>
2018-06-28 11:46:07 -0500

    Jquery validator added for Files form

--- resources/views/files/create.blade.php
+++ resources/views/files/create.blade.php
@@ -15,7 +15,7 @@
               </div>
             </div><hr>
 
-            {{ Form::open(['url' => 'files', 'class' => 'forms']) }}
+            {{ Form::open(['url' => 'files', 'class' => 'forms', 'id' => 'createFiles']) }}
 
             @csrf
             <div class="row">
@@ -23,10 +23,64 @@
             </div>
 
             {{ Form::close() }}
-            
+
           </div>
         </div>
       </div>
     </div>
   </div>
+@endsection
+
+
+@section('script')
+  <script>
+    $(() => {
+      $('#createFiles').validate({
+        rules: {
+          program_id: {
+            required: true
+          },
+          characterization_id: {
+            required: true
+          },
+          file_number: {
+            required: true,
+            digits: true,
+            maxlength: 25
+          },
+          file_route: {
+            required: true,
+            maxlength: 30
+          },
+          apprentices: {
+            required: true,
+            digits: true,
+            maxlength: 25
+          },
+        },
+        messages: {
+          program_id: {
+            required: "Debes elegir un Programa de Formación.",
+          },
+          characterization_id: {
+            required: "Debes elegir una Caracterización.",
+          },
+          file_number: {
+            required: "El campo Número de Ficha debe existir.",
+            digits: "El campo Número de Ficha debe ser numérico.",
+            maxlength: "El campo Número de Ficha debe contener máximo 25 caracteres."
+          },
+          file_route: {
+            required: "El campo Ruta de Aprendizaje debe existir.",
+            maxlength: "El campo Ruta de Aprendizaje debe contener máximo 30 caracteres."
+          },
+          apprentices: {
+            required: "El campo Aprendices debe existir.",
+            digits: "El campo Aprendices debe ser numérico.",
+            maxlength: "El campo Aprendices debe contener máximo 25 caracteres."
+          },
+        }
+      });
+    });
+  </script>
 @endsection
